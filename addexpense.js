6664879a813cc701d89039a5a4818d3042276796
serenawens <serenawens@gmail.com>
2023-02-26 00:16:11 -0500

    Update addexpense.js

--- addexpense.js
+++ addexpense.js
@@ -1,4 +1,5 @@
 const firebaseConfig = {
+<<<<<<< Updated upstream
     apiKey: "AIzaSyA7G8lJl_6mHTfIDcNXwRsLurGAeY23YsU",
     authDomain: "devfest-db40c.firebaseapp.com",
     databaseURL: "https://devfest-db40c-default-rtdb.firebaseio.com",
@@ -55,12 +56,74 @@ const firebaseConfig = {
       snapshot.forEach(function(childSnapshot) {
         var username = childSnapshot.key;
         var name = childSnapshot.key.value();
+=======
+  apiKey: "AIzaSyA7G8lJl_6mHTfIDcNXwRsLurGAeY23YsU",
+  authDomain: "devfest-db40c.firebaseapp.com",
+  databaseURL: "https://devfest-db40c-default-rtdb.firebaseio.com",
+  projectId: "devfest-db40c",
+  storageBucket: "devfest-db40c.appspot.com",
+  messagingSenderId: "974911936807",
+  appId: "1:974911936807:web:7ea2b7b8f510f2691cf102",
+  measurementId: "G-HX1LXBLZR5"
+};
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+// reference your database
+var expenseFormDB = firebase.database().ref("Expenses");
+var userDB = firebase.database().ref("Users");
+
+document.getElementById("expenseForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  var expense = getElementVal("expense");
+  var payer = getElementVal("payer");
+  var date = getElementVal("date");
+  var amount = getElementVal("amount");
+  var type = getElementVal("type");
+  var splitMethod = document.querySelector('#split').value;
+
+  saveExpenseInfo(expense, payer, date, amount, type, splitMethod);
+  document.getElementById("expenseForm").reset();
+
+  window.location.replace("profile.html");
+}
+
+const saveExpenseInfo = (expense, payer, date, amount, type, splitMethod) => {
+
+  var owe = {};
+  userDB.once("value") //change?
+    .then(function (snapshot) {
+      var numParticipants = snapshot.numChildren();
+      var payment = parseInt(amount) / numParticipants;
+
+      snapshot.forEach(function (childSnapshot) {
+        var user = childSnapshot.value;
+        var username = childSnapshot.key;
+        var name = user["name"];
+        if (username != payer) {
+          owe.add({[username]: {name: name, amount: payment}});
+        }
+      });
+    });
+
+
+  var newExpenseForm = expenseFormDB.push().set({
+    name: expense,
+    participants: owe,
+    type: type,
+    date: date,
+    total: amount,
+    splitMethod: splitMethod
+>>>>>>> Stashed changes
   });
-});
 
-    var participants = new Set();
-    if (splitMethod.equals("evensplit")){
+};
 
+<<<<<<< Updated upstream
     }
 
   };
@@ -78,3 +141,8 @@ const firebaseConfig = {
     let formNameQP = "name="+current_username;
     window.location.replace("profile.html?"+formNameQP);
   }
+=======
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
+>>>>>>> Stashed changes
